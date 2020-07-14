f0dc4f03e757fc03aabb81c9045aeedd43b3cb9c
TeamAnonyme69 <63798785+TeamAnonyme69@users.noreply.github.com>
2020-07-14 14:28:33 +0200

    Update main.cxx

--- WorldChange/main.cxx
+++ WorldChange/main.cxx
@@ -3,7 +3,27 @@
 using namespace std ;
 
 int main(){
+  
+  int choice :
+  
   cout<<("WorldChange version beta test v0.1.1publicBETA \n");
-
+  cout<<("choice :\n");
+  cout<<("1 : Glass world\n");
+  cout<<("2 : Black world\n");
+  cout<<("3 : Mineral world\n");
+  cout<<("4 : Pastel world ( wool world )\n");
+  cout<<("5 : Exit\n");
+  cout<<("YOUR CHOICE :"); 
+  cin>>choice ;
+  if (choice == 1){
+    system("python3 glassWorld.py");
+  }
+  else if (choice == 2){
+    system("python3 blackWorld.py");
+  }
+  else if (choice == 3{
+    system("python3 mineralWorld.py");
+  }
+  return 0; 
 
 }
