6d0b3ba66301fc410420ae091834d41dc47ed8ed
Eric42053 <63798785+TeamAnonyme69@users.noreply.github.com>
2020-07-14 16:58:22 +0200

    Create stoneWorld.py

---
+++ WorldChange/stoneWorld.py
@@ -0,0 +1,22 @@
+from mcpi.minecraft import Minecraft
+
+mc = Minecraft.create()
+
+x = 127
+y = 64
+z = 127
+mc.postToChat("Dr stone's world")
+for a in range(256):
+  z = 127
+  for b in range(256):
+    y = mc.getHeight(x,z)
+    y = y + 64
+    for c in range(y):
+      bl = mc.getBlock(x, y, z)
+      if bl == 0 :
+        pass
+      else :
+        mc.setBlock(x,y,z,1)
+      y = y - 1
+    z = z - 1
+  x = x - 1
