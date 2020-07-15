1bb9e067dd0cfe5dd7d224a6b29f0fa974c04b99
Eric42053 <63798785+TeamAnonyme69@users.noreply.github.com>
2020-07-15 11:24:30 +0200

    Update paneWorld.py

--- WorldChange/paneWorld.py
+++ WorldChange/paneWorld.py
@@ -1,1 +1,24 @@
 
+
+from mcpi.minecraft import Minecraft
+
+mc = Minecraft.create()
+
+x = 127
+y = 64
+z = 127
+
+for a in range(256):
+  z = 127
+  for b in range(256):
+    y = mc.getHeight(x,z)
+    y = y + 64
+    for c in range(y):
+      bl = mc.getBlock(x, y, z)
+      if bl == 0 :
+        pass
+      else :
+        mc.setBlock(x,y,z,102)
+      y = y - 1
+    z = z - 1
+  x = x - 1
