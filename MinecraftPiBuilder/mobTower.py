61a402f30f57f4bd403343282dc6dfdc67091c29
Eric42053 <63798785+TeamAnonyme69@users.noreply.github.com>
2020-07-15 12:31:15 +0200

    Update mobTower.py

--- MinecraftPiBuilder/mobTower.py
+++ MinecraftPiBuilder/mobTower.py
@@ -1,2 +1,15 @@
 from mcpi.minecrft import Minecraft
 from time import sleep
+
+mc = Minecraft.create()
+
+#CREATE A MOB TOWER : LEVEL 1
+
+mc.setBlocks(-20, 0,-20, 20, 0, 20,0)
+sleep(2)
+
+mc.setBlocks(-5, 0,-5,-5 30 , 5, 4)
+mc.setBlocks(5, 0,-5,-5 30 , 5, 4)
+mc.setBlocks(5, 0,-5,-5 30 , -5, 4)
+mc.setBlocks(-5, 0,5,-5 30 , 5, 4)
+
