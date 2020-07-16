bab7cb4189f7fdcb193b2e677ba06821560ffcbb
Eric42053 <63798785+TeamAnonyme69@users.noreply.github.com>
2020-07-16 11:51:25 +0200

    Updated a version of Floor is lava on Minecraft pi

--- Minigames/FloorIsLava_parkour.py
+++ Minigames/FloorIsLava_parkour.py
@@ -3,3 +3,30 @@
 
 from mcpi.minecraft import Minecraft
 from time import sleep
+from random import randint
+
+mc = Minecraft.create()
+
+def walls():
+  mc.setBlocks(-20, 0, -20, -20, 20 ,20, 246)
+  sleep(3)
+  mc.setBlocks(-20, 0, -20, 20, 20, -20, 246)
+  sleep(3)
+  mc.setBlocks(20, 0, 20, -20, 20, 20, 246)
+  sleep(3)
+  mc.setBlocks(20, 0, 20, 20, 20, -20, 246)
+  sleep(3)
+  mc.setBlocks(-20, -1, -20, 20, -1, 20, 246)
+  sleep(3)
+  
+def lava(y):
+  mc.setBlocks(19, y, 19, -19, y, -19, 10)
+  
+def wait(s): #le temps dans la cage de depart
+  sa = s / 10
+  sb = s*10
+  for a in range(sb):
+    color = randint(1, 14)
+    mc.player.setPos(0, 3, 0)
+    mc.setBlock(0, 2, 0, 35, color)
+    sleep(sa)
